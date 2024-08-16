b985febec4e45f680d567416435fd277b360cf71
Tatianacrouse <tatianadjeutio@gmail.com>
2024-08-16 08:33:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -74,3 +74,11 @@ function validateEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
+emailjs.send('service_akxgt0n', 'template_hn4gplz', data)
+    .then((response) => {
+        // ...
+    })
+    .catch((error) => {
+        // ...
+    });
+
